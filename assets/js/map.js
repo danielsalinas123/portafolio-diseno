45e365cf4e2bb3a267250bf21cc7bcb4795b9dda
Daniel Salinas Jaramillo <leinad.salinsj@gmail.com>
2026-01-20 13:42:32 -0500

    feat: maquetación inicial de escritorio con CSS completo

---
+++ assets/js/map.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded",()=>
+{
+    setTimeout(()=>
+    {
+        document.querySelector("#load-iframe-map").innerHTML=`<iframe class="contact__iframe" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" loading="lazy" referrerpolicy="no-referrer-when-downgrade" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3036.004753794616!2d-3.6906815246454174!3d40.453031754292894!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd4228e23705d39f%3A0xa8fff6d26e2b1988!2zQmVybmFiw6l1!5e0!3m2!1ses!2sco!4v1768878297759!5m2!1ses!2sco"></iframe>`;
+    },1000);
+});
